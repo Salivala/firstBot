4704fcabcdd29c903a107a4297cbff9b93f53255
hsszyman <hsszyman@gmail.com>
2018-10-30 16:48:07 -0400

    Added a more elegant way of ending the program. Will likely try to have the bot run as a background process.

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-import sys
-import praw
-import pdb
-import re
-import os
 from FileWrapper import FileWrapper
 from bot import Bot
 
-print("starting")
-while True:
+for i in range(1, 6):
     file_wrapper: FileWrapper = FileWrapper(filename="replied_posts.txt")
     bot: Bot = Bot(subreddit_name="toundrabotplayground", bot_name="bot1", file=file_wrapper)
     for comment in bot.find_comments_by_content(contains="\[reverse\]"):
-        print("wow!" + comment.id)
+        print("Replied to " + comment.id)
         comment.body = comment.body[9:]
         comment.reply(comment.body[::-1])
+exit("program terminated safely")
 
